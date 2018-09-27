b08a1539469276849fc0dae28f58c1e46fbc0f55
Scott <scrosehart@gmail.com>
2018-09-26 22:42:51 -0700

    working on addMatch page still

--- Components/AddMatch.js
+++ Components/AddMatch.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Dimensions, Animated, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Animated, DatePickerIOS, Keyboard, TouchableWithoutFeedback, Text, TextInput, View } from 'react-native';
+import Modal from 'react-native-modalbox'
+import DatePicker from './DatePicker';
 import { Icon } from 'react-native-elements';
 
-const slideTime = 850;
+const slideTime = 700;
 
 export default class AddMatch extends Component {
-  state = {
-    slideAnim: new Animated.Value(Dimensions.get('window').width)
+  constructor(props) {
+    super(props)
+    this.state = {
+      slideAnim: new Animated.Value(Dimensions.get('window').width),
+      showModal: false,
+      date: new Date(),
+      score: '0'
+    }
+  }
+
+  flipModal = bool => {
+    this.setState( {showModal: bool} )
+  }
+
+  setDate = newDate => {
+    this.setState( {date: newDate} );
   }
 
   animateClose = () => {
@@ -34,19 +50,53 @@ export default class AddMatch extends Component {
     let { slideAnim } = this.state;
     return (
       <Animated.View style={[
-        styles.addMatches,
+        styles.addMatchesWrapper,
         { transform: [ {translateX: slideAnim} ]}
       ]}>
-        <Text>add match</Text>
-        <Text onPress={this.animateClose}>~~~Close~~~</Text>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.addMatchesView}>
+          <Text onPress={this.animateClose}>~~~Close~~~</Text>
+          <Text>CourseName</Text>
+          <Text onPress={() => this.flipModal(true)}>
+            {this.state.date.toDateString()} {this.state.date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}
+          </Text>
+          <Modal
+            style={styles.modal}
+            isOpen={this.state.showModal}
+            backdropPressToClose={true}
+            position='bottom'
+            backdrop={true}
+            animationDuration={500}
+            onClosed={() => this.flipModal(false)}
+            >
+              <DatePicker date={this.state.date} setDate={this.setDate} />
+          </Modal>
+          <Text>Score</Text>
+          <TextInput
+            value={this.state.score}
+            onChangeText={score => this.setState({score})}
+            keyboardType='numeric'
+            maxLength={3}
+          />
+          <Text>Price</Text>
+          <Text>Notes_____________</Text>
+        </View>
+      </TouchableWithoutFeedback>
       </Animated.View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  addMatches: {
-    backgroundColor: 'yellow',
-    top: 0
+  addMatchesWrapper: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'yellow'
+  },
+  addMatchesView: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  modal: {
+    paddingTop: '5%',
+    height: '40%'
   }
 });
---
+++ Components/DatePicker.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text, DatePickerIOS } from 'react-native';
+
+export default class DatePicker extends React.Component {
+  render() {
+    return (
+      <View style={styles.datePicker}>
+        <DatePickerIOS date={this.props.date} onDateChange={this.props.setDate} minuteInterval={5} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  datePicker: {
+    // backgroundColor: 'red',
+  }
+});
